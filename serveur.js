c3331814f5d8c8353e6fdb9b359c23d1f59c511e
EpicZazaMan <evan1998@hotmail.fr>
2019-03-27 11:14:28 +0100

    creation fonctionrecup et fction user

--- serveur.js
+++ serveur.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const motDePasse = "P@ssw0rd";
 
+//User require-------------------------------------------------------------------------------------------------------
+const { postUser} =  require("./fonctionRecup/userFonction/post");
+const { getUsers,getUserById } =  require("./fonctionRecup/userFonction/get");
+const { putUser} =  require("./fonctionRecup/userFonction/put");
+const { delUser} =  require("./fonctionRecup/userFonction/del");
+
+
 
 //Routing-------------------------------------------------------------------------------------------------------
 app.use(bodyParser.json());
@@ -13,3 +20,19 @@ app.get("/",(req,res)=>{
 console.log('Nous disposons votre serveur sur le port:3000');
 app.listen(3000);
 
+// User routing---------------------------------------------------------------------------------------------
+app.post('/', (req, res) => {
+    postUser(req,res);
+ });
+ app.get('/user',(req,res)=>{
+     getUsers(req,res);
+ });
+ app.get('/user/:id',(req,res)=>{
+     getUserById(req,res);
+ });
+ app.put('/user/:id',(req,res)=>{
+     putUser(req,res);
+ });
+ app.delete('/user/:id',(req,res)=>{
+     delUser(req,res);
+ });
