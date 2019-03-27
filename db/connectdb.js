3317a2ceef04983a67e891bdbab4694fe4f7b261
EpicZazaMan <evan1998@hotmail.fr>
2019-03-27 10:01:30 +0100

    connectdb js + creation db

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
-
-const uri = `mongodb+srv://admin:P@ssw0rd@cluster0-3kwrt.mongodb.net/BFB?retryWrites=true`;
+const mongoose = require("mongoose");
+const uri = "mongodb+srv://Admin:<P@ssw0rd>@cluster0-sm8kp.mongodb.net/test?retryWrites=true";
 
 mongoose.Promise = global.Promise;
 mongoose.connect(uri, { useNewUrlParser: true });
 
 module.exports = { mongoose }
+
