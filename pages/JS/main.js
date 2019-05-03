b66833dcdc4e0e43813f2b1c928b14513bb07d7f
EpicZazaMan <evan1998@hotmail.fr>
2019-05-03 08:27:02 +0200

    léo jle déteste

--- pages/JS/main.js
+++ pages/JS/main.js
@@ -6,4 +6,17 @@ fetch("http://localhost:3000/fournisseur")
     })
     .then(function (data) {
         console.log(data);
+        var i=0;
+        data.forEach(element => {
+            var h = document.createElement("h1");
+            var t = document.createTextNode(data[i].nom);
+            h.appendChild(t);
+            document.body.appendChild(h);
+            i++;
+        });
+        document.getElementsByTagName("h1").addClass("prout");
     });
+
+
+
+    var header = document.querySelector('header');
